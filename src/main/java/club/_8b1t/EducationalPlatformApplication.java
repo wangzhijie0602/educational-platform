e03ae344e1942825124b339030cfcf5f8bbadd55
wangzhijie0602 <46142484+wangzhijie0602@users.noreply.github.com>
2024-11-05 13:33:46 +0800

    Hello educational-platform

---
+++ src/main/java/club/_8b1t/EducationalPlatformApplication.java
@@ -0,0 +1,13 @@
+package club._8b1t;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationalPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducationalPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/club/_8b1t/EducationalPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package club._8b1t;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationalPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
