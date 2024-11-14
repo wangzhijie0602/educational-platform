e5e13842966dfb2efe3931f8f3258605b325cb6f
wangzhijie0602 <46142484+wangzhijie0602@users.noreply.github.com>
2024-11-14 11:39:32 +0800

    引入aop实现日志记录

--- src/main/java/club/_8b1t/controller/AuthController.java
+++ src/main/java/club/_8b1t/controller/AuthController.java
@@ -4,7 +4,6 @@ import club._8b1t.utils.ResultUtil;
 import club._8b1t.pojo.User;
 import club._8b1t.service.UserService;
 import club._8b1t.utils.JwtUtil;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,7 +14,6 @@ import org.springframework.web.bind.annotation.*;
  * @version 1.0
  * @since 1.0
  */
-@Slf4j
 @RestController
 public class AuthController {
 
@@ -24,7 +22,6 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResultUtil userLogin(@RequestBody User user) {
-        log.info("用户登录: {}", user);
         User u = userService.login(user);
 
 //        用户登录成功,生成并下发令牌
@@ -38,7 +35,6 @@ public class AuthController {
 
     @PostMapping("/register")
     public ResultUtil userRegister(@RequestBody User user) {
-        log.info("用户注册: {}", user);
         return userService.register(user) ? ResultUtil.success() : ResultUtil.error("注册失败!");
     }
 
